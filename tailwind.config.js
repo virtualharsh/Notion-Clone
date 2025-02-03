ab81778522495410cf1435c4c43508d791f027ca
harsh <harshsonegrahere@gmail.com>
2025-02-03 17:44:30 +0530

    fixed sidebar items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // ensure this path includes your React components
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // ensure this path includes your React components
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                quicksand: ["Quicksand", "sans-serif"],
+                cutive: ["Cutive Mono", "monospace"],
+                kirang: ["Kirang Haerang", "cursive"],
+            },
+        },
+    },
+    plugins: [],
 };
